ea01cd033b15b9454782853304e22060589cb9ac
olegsavchuk22 <122669271+olegsavchuk22@users.noreply.github.com>
2023-06-01 20:39:04 +0300

    Добавьте файлы проекта.

---
+++ DZ3_Savchuk/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DZ3_Savchuk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.load_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(775, 363);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // load_btn
+            // 
+            this.load_btn.Location = new System.Drawing.Point(12, 401);
+            this.load_btn.Name = "load_btn";
+            this.load_btn.Size = new System.Drawing.Size(114, 37);
+            this.load_btn.TabIndex = 1;
+            this.load_btn.Text = "Load";
+            this.load_btn.UseVisualStyleBackColor = true;
+            this.load_btn.Click += new System.EventHandler(this.load_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.load_btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button load_btn;
+    }
+}
+
---
+++ DZ3_Savchuk/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DZ3_Savchuk
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async Task LoadBooksData()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection())
+                {
+                    string cs = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+                    conn.ConnectionString = cs;
+                    await conn.OpenAsync();
+
+                    string query = @"SELECT b.*, c.Category, i.Izd, t.Themes, f.Format
+                                     FROM books_new b
+                                     LEFT JOIN Spr_kategory c ON b.kategory_id = c.id
+                                     LEFT JOIN Spr_izd i ON b.izd_id = i.id
+                                     LEFT JOIN Spr_themes t ON b.themes_id = t.id
+                                     LEFT JOIN Spr_format f ON b.format_id = f.id";
+
+                    SqlCommand command = new SqlCommand(query, conn);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    await Task.Run(() => adapter.Fill(dataTable));
+
+                    dataGridView1.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка під час завантаження даних: " + ex.Message);
+            }
+        }
+
+        private async void load_btn_Click(object sender, EventArgs e)
+        {
+            await LoadBooksData();
+        }
+    }
+}
